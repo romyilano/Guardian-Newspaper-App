f2679f471fbaa479b31cf8e3dd392b65c55345db
Romy Ilano <romy@snowyla.com>
2013-12-06 08:48:56 -0800

    basic IB setup

--- Guardian Networking Sample/ViewController.h
+++ Guardian Networking Sample/ViewController.h
@@ -4,10 +4,14 @@
 //
 //  Created by Romy Ilano on 12/6/13.
 //  Copyright (c) 2013 Romy Ilano. All rights reserved.
-//
+// Abstract: simple view controller to show latest iphone related
+// articles on guardian. Just using this as a test class
 
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UIButton *articleButton;
+- (IBAction)summonarticlebtnPressed:(UIButton *)sender;
 
 @end
