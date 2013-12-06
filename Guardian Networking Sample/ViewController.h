98d2295a66ed6e1962df5b017ce6f95edec55a3b
Romy Ilano <romy@snowyla.com>
2013-12-06 08:17:56 -0800

    first

---
+++ Guardian Networking Sample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Guardian Networking Sample
+//
+//  Created by Romy Ilano on 12/6/13.
+//  Copyright (c) 2013 Romy Ilano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Guardian Networking SampleTests/Guardian_Networking_SampleTests.m
@@ -0,0 +1,34 @@
+//
+//  Guardian_Networking_SampleTests.m
+//  Guardian Networking SampleTests
+//
+//  Created by Romy Ilano on 12/6/13.
+//  Copyright (c) 2013 Romy Ilano. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Guardian_Networking_SampleTests : XCTestCase
+
+@end
+
+@implementation Guardian_Networking_SampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
