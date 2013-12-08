f33dcb6767d44866bba79415de0ab108fd8b244c
Romy Ilano <romy@snowyla.com>
2013-12-08 12:05:40 -0800

    added frameworks like coretext, quartzcore, etc

---
+++ Guardian Networking Sample/ResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultsViewController.h
+//  Guardian Networking Sample
+//
+//  Created by Romy Ilano on 12/8/13.
+//  Copyright (c) 2013 Romy Ilano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultsViewController : UITableViewController
+
+@end
