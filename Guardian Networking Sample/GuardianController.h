e8e3c21c82796b3abe33b361930edd1ecc118e4a
Romy Ilano <romy.ilano@gmail.com>
2013-12-21 07:43:02 -0800

    precompiled headers

---
+++ Guardian Networking Sample/GuardianController.h
@@ -0,0 +1,13 @@
+//
+//  GuardianController.h
+//  Guardian Networking Sample
+//
+//  Created by Romy Ilano on 12/21/13.
+//  Copyright (c) 2013 Romy Ilano. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GuardianController : NSObject
+
+@end
---
+++ Guardian Networking Sample/GuardianController.m
@@ -0,0 +1,13 @@
+//
+//  GuardianController.m
+//  Guardian Networking Sample
+//
+//  Created by Romy Ilano on 12/21/13.
+//  Copyright (c) 2013 Romy Ilano. All rights reserved.
+//
+
+#import "GuardianController.h"
+
+@implementation GuardianController
+
+@end
