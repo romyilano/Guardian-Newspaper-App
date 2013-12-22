50ed8a6da4db838b8b028946aae0b9cff4fe836c
Romy Ilano <romy.ilano@gmail.com>
2013-12-21 16:33:57 -0800

    added init: and sharedController method

--- Guardian Networking Sample/GuardianController.h
+++ Guardian Networking Sample/GuardianController.h
@@ -9,5 +9,5 @@
 #import <Foundation/Foundation.h>
 
 @interface GuardianController : NSObject
-
++ (id)sharedController;
 @end
--- Guardian Networking Sample/GuardianController.m
+++ Guardian Networking Sample/GuardianController.m
@@ -7,7 +7,29 @@
 //
 
 #import "GuardianController.h"
+#import "AFNetworking/AFNetworking.h"
 
 @implementation GuardianController
++ (id)sharedController
+{
+    static dispatch_once_t onceToken;
+    static GuardianController *guardianController;
+    dispatch_once(&onceToken, ^{
+        
+        // to-do ask
+        // difference between self alloc / GuardianController malloc
+        guardianController = [[self alloc] init];
+    });
+    return guardianController;
+}
+
+-(id)init
+{
+    self = [super init];
+    if (!self) return nil;
+    [[AFNetworkActivityIndicatorManager sharedManager] setEnabled:YES];
+    
+    return self;
+}
 
 @end
